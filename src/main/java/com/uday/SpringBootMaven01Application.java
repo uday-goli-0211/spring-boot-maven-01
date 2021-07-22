d0a860426ecf8ae30a3f6973b4234e13d9df5db9
uday goli <rohan.maheshbabu@gmail.com>
2021-07-22 22:13:10 +0530

    added mvn proj

---
+++ src/main/java/com/uday/SpringBootMaven01Application.java
@@ -0,0 +1,13 @@
+package com.uday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMaven01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMaven01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uday/SpringBootMaven01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMaven01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
